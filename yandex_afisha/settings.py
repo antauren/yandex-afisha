606f4f879344e411f848823fc766f2ea2b8122d3
Anatoly <antauren@yandex.ru>
2020-06-03 06:37:13 +0300

    Rename app

--- yandex_afisha/settings.py
+++ yandex_afisha/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'afisha.apps.AfishaConfig',
+    'places.apps.PlacesConfig',
 ]
 
 MIDDLEWARE = [
