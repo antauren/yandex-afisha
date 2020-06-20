4e88684f992c953a63e23ffd68b23695acaebf0b
antauren <antauren@yandex.ru>
2020-06-20 21:58:54 +0300

    add preview

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
+from django.utils.html import format_html, mark_safe
 
 from .models import Place, Image
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+    readonly_fields = ['get_preview']
+
+    def get_preview(self, obj):
+        html = '<img src="{url}" height="{height}"/>'.format(url=obj.image.url,
+                                                             height=200,
+                                                             )
+        return format_html('{}', mark_safe(html))
 
 
 @admin.register(Place)
