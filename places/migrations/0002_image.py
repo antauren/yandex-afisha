c7eeadac1468fbd404d6d901784096696cc03d3e
antauren <antauren@yandex.ru>
2020-08-24 03:17:25 +0300

    reformat

--- places/migrations/0002_image.py
+++ places/migrations/0002_image.py
@@ -16,8 +16,14 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(blank=True, max_length=200, verbose_name='Название')),
                 ('image', models.ImageField(upload_to='images', verbose_name='Изображение')),
-                ('place', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE,
-                                            related_name='images', to='places.Place', verbose_name='Место')),
+                ('place', models.ForeignKey(
+                    blank=True,
+                    null=True,
+                    on_delete=django.db.models.deletion.CASCADE,
+                    related_name='images',
+                    to='places.Place',
+                    verbose_name='Место'
+                )),
             ],
         ),
     ]
