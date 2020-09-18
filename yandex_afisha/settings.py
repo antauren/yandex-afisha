4aad9706976640a2717b1c160017c980e7562a84
Anatoly <antauren@yandex.ru>
2020-09-18 06:11:20 +0300

    fix getting SECRET_KEY

--- yandex_afisha/settings.py
+++ yandex_afisha/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY', 'REPLACE_ME')
+SECRET_KEY = env.str('SECRET_KEY', 'REPLACE_ME')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', False)
